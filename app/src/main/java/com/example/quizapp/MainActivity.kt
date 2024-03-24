c65459ed2d7e5a72d8f6957f0af6ef7d061a83c8
FaisalAbubaker <faiabu95@gmail.com>
2024-03-25 00:19:54 +0300

    message

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -15,15 +15,20 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RangeSlider
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.Role.Companion.Button
@@ -89,6 +94,36 @@ fun Quiz(
         }
         item{
             Text(text = viewModel.score.value)
+        }
+        item{
+            Column {
+                var sliderPosition by remember { mutableFloatStateOf(0f) }
+                Column {
+                    Slider(
+                        enabled = false,
+                        value = sliderPosition,
+                        onValueChange = { sliderPosition = it }
+                    )
+                    Text(text = sliderPosition.toString())
+                }
+
+                var sliderPosition2 by remember { mutableStateOf(0f..100f) }
+                Column {
+                    RangeSlider(
+                        value = sliderPosition2,
+                        steps = 5,
+                        onValueChange = { range -> sliderPosition2 = range },
+                        valueRange = 0f..100f,
+                        colors = SliderDefaults.colors(
+                            thumbColor = MaterialTheme.colorScheme.secondary,
+                            activeTrackColor = MaterialTheme.colorScheme.secondary,
+                            inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+                        ),
+
+                    )
+                    Text(text = sliderPosition2.toString())
+                }
+            }
         }
     }
 }
